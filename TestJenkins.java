adf5afac0fbd9d53eae959bb4e81965fced21904
Shaik Subhani <ssubhanishaik6@gmail.com>
2026-04-22 14:42:16 +0530

    TestJenkins.java files was commited

---
+++ TestJenkins.java
@@ -0,0 +1,13 @@
+public class TestJenkins {
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new java.util.Date());
+    }
+}
+
+
+
+
+
+
